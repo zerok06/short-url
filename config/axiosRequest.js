141fbb1bda33443c1df9fc519822ff7aedf24331
ZeroK ZK <Jose.GeeksJose@gmail.com>
2023-03-30 15:24:24 -0500

    1.3.1.1

--- config/axiosRequest.js
+++ config/axiosRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VERCEL_URL || "http://localhost:3000/api/",
+  baseURL: process.env.VERCEL_URL + "/api/" || "http://localhost:3000/api/",
   timeout: 3000,
 });
 
