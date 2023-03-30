d359c171a55ffaab5283530dd8c9c31ca2b8cd29
ZeroK ZK <Jose.GeeksJose@gmail.com>
2023-03-29 19:42:14 -0500

    1.2.7

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import Qr from "../components/Qr";
 import useQrState from "../store/useQrStore";
 export default function Home() {
   const { urlShort, setUrlShort } = useQrState((state) => state);
+  console.log(process.env.VERCEL_URL);
 
   return (
     <>
